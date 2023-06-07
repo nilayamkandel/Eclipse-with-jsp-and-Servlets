be19ba4bf70740cbfeb0b87cb679fbb0c5b78077
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-07 14:32:23 +0545

    perf: login form servlet

---
+++ WebDemo/src/main/java/com/servlets/LoginServlet.java
@@ -0,0 +1,48 @@
+package com.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+       
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("LoginForm.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String un = request.getParameter("username");
+		String pw = request.getParameter("password");
+		
+			if(un.equals("ram")&&pw.equals("123")) {
+				request.setAttribute("uname", "pw");
+				request.getRequestDispatcher("Home.jsp").forward(request,response);
+			}else {
+				request.setAttribute("error", "user not found !!");
+				request.getRequestDispatcher("LoginForm.jsp").forward(request,response);
+			}
+		
+	}
+
+}
